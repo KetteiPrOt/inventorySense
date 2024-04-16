a998465e011cf138f73d6b279a1678e44c1efe67
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-04-16 14:41:28 -0500

    Added (small change) new default roles

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -17,6 +17,8 @@ class RolesSeeder extends Seeder
         /// Roles
         Role::create(['name' => Role::$superAdmin]);
         $seller = Role::create(['name' => 'Vendedor']);
+        $creditController = Role::create(['name' => 'Controlador de crédito']);
+        $sentinel = Role::create(['name' => 'Centinela']);;
 
         // Permissions
         foreach(Permission::$permissions as $permission){
@@ -25,10 +27,27 @@ class RolesSeeder extends Seeder
 
         // Assign Permissions to Roles
         $sellerPermissions = [
-            'create-sales'
+            'create-sales',
+            'cash-closing',
+            'sales-report'
         ];
         foreach($sellerPermissions as $sellerPermission){
             $seller->givePermissionTo($sellerPermission);
+        }
+        $creditControllerPermissions = [
+            'create-sales',
+            'cash-closing',
+            'see-all-incomes',
+            'sales-report',
+            'see-all-sales',
+            'edit-all-sales'
+        ];
+        foreach($creditControllerPermissions as $creditControllerPermission){
+            $creditController->givePermissionTo($creditControllerPermission);
+        }
+        $sentinelPermissions = Permission::$directPermissions;
+        foreach($sentinelPermissions as $sentinelPermission){
+            $sentinel->givePermissionTo($sentinelPermission);
         }
     }
 }
